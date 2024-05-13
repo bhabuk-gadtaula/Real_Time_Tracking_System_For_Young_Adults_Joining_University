28580fe0c77b043ea95ad8eceaef7aa1e1823590
Bhabuk Gadtaula <gadtaulabhabuk7@gmail.com>
2024-05-13 18:26:26 +0100

    project initialize

---
+++ src/core/location/api-route/index.ts
@@ -0,0 +1,18 @@
+import { BASE_URL } from '../../../shared/constant/base-url';
+import { ApiMethod } from '../../../shared/enums/api-method';
+
+export const locationRoute = {
+  createLocation: {
+    method: ApiMethod.POST,
+    route: BASE_URL + '/locations',
+  },
+
+  findAllLocations: {
+    method: ApiMethod.GET,
+    route: BASE_URL + '/locations',
+  },
+  calculateDistance: {
+    method: ApiMethod.POST,
+    route: BASE_URL + '/locations/calculate-distance',
+  },
+};
---
+++ src/shared/enums/api-method.ts
@@ -0,0 +1,12 @@
+export enum ApiMethod {
+  POST = 'POST',
+  GET = 'GET',
+  PATCH = 'PATCH',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
+
+export enum ProjectModule {
+  USER = 'user',
+  CLASS = 'class',
+}
